fbff78fb59c8bc6768fd34862ceb81506c58cb37
angalet <41893557+angalet@users.noreply.github.com>
2018-08-14 12:49:40 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Инфо карта</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        section{
+            margin:auto;
+            max-width: 70%;
+            text-align: center;
+        }
+        
+        section p{
+            font-size: 21px;
+            font-weight:600; 
+        }
+
+         section h1{
+             color: blue;
+        }
+
+        ul{
+            text-align:  left;
+        }
+    </style>
+</head>
+
+<body>
+    <section>
+<?php
+$number = rand(4,9999);
+$myname = 'Богдан';
+$mysurname = 'Ангалейт';
+$myage = 34;
+$mycity = 'Москва';
+$myemail = 'angaleit@gmail.com';
+$myinfo = 'Работаю в IT компании. Люблю спорт, путешествия, читать.
+Учусь на онлайн-курсах в Нетологии.';
+echo '<h1>Информационная карточка - '.$myname.'а. Дата '.date('D M Y').'</h1>'; //Просто чтоб вывести разным способами
+echo "<p>номер карточки - $number<p>";
+?>
+<ul>
+    <li>Полное имя - <?=$mysurname." ".$myname;?></li>
+    <li>Мой возраст - <?=$myage;?> года</li>
+    <li>Город проживания - <?=$mycity;?></li>
+    <li>E-mail - <?=$myemail;?></li>
+    <li>Информация обо мне - <?=$myinfo;?></li>
+</section>
+</body>
+</html>
